39290d51b399f66c497d1d9a476b382a2f518303
Michal Bukovsky <michal.bukovsky@firma.seznam.cz>
2019-06-10 12:17:45 +0200

    #! v4.0.3~burloglibteng4-1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if os.system(pattern.replace('XXX', boost_python)) != 0:
 print('checking boost_python library name: ' + boost_python)
 
 setup(name="teng",
-      version="4.0.3",
+      version="4.0.3~burloglibteng4",
       description="Teng python module written on boost python.",
       author="Michal Bukovsky",
       author_email="burlog@seznam.cz",
