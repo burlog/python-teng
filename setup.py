396432a5855834e8c30bc54077e5f42183a30648
Michal Bukovsky <michal.bukovsky@firma.seznam.cz>
2019-11-05 12:45:52 +0100

    fix

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ boost_python = 'boost_python-py%d%d' % (version[0], version[1])
 if os.system(pattern.replace('XXX', boost_python)) != 0:
     boost_python = 'boost_python-%d.%d' % (version[0], version[1])
     if os.system(pattern.replace('XXX', boost_python)) != 0:
-        boost_python = 'boost_python-%d%d' % (version[0], version[1])
+        boost_python = 'boost_python%d%d' % (version[0], version[1])
         if os.system(pattern.replace('XXX', boost_python)) != 0:
             print('can\'t find boost_python library')
             sys.exit(1)
